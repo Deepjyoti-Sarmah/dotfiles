6a76aa26bb756be9f95af4f9576348cf8dbb32dd
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-09-08 12:18:01 +0530

    fixing settings and autosave

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup("AutoSave", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = augroup,
+  callback = function()
+    if vim.bo.modifiable and vim.bo.modified then
+      vim.cmd("silent! write")
+      print("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+    end
+  end,
+})
